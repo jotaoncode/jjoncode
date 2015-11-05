e6b90232b38b7d758db7081c427268a410249ce1
jotaoncode <garcia_juan_j@yahoo.com.ar>
2015-11-05 10:23:57 -0300

    Added index page for jota on code website

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,7 @@
 var routing = require('./routing');
 
 function Trends (req, res) {
-  var pathPage = __dirname + "/pages/home/index.html";
+  var pathPage = __dirname + "/pages/index.html";
   routing(req, res, pathPage);
 }
 module.exports = Trends;
